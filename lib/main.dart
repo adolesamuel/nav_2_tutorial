921977a9cc5e43accf37186ae99ddac52d24ec19
Adole Samuel Sani <adolesamuel4@gmail.com>
2022-11-02 20:32:10 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BooksApp());
+}
+
+class Book {
+  final String title;
+  final String author;
+
+  Book(this.title, this.author);
+}
+
+class BooksApp extends StatefulWidget {
+  const BooksApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Books App',
+      home: Navigator(
+        pages: const [
+          MaterialPage(
+            key: ValueKey('BooksListPage'),
+            child: Scaffold(),
+          )
+        ],
+        onPopPage: (route, result) => route.didPop(result),
+      ),
+    );
+  }
+}
